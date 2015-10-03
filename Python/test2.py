4c39632f3fecc0f272a32994aa30f8d382d6a991
sharanrc <rc.sharanaprasad@gmail.com>
2015-10-03 21:54:11 +0530

    googleapi

--- Python/test2.py
+++ Python/test2.py
@@ -2,17 +2,9 @@
 import re
 import time
 import getch
-import kbh
 from googleapi import getQuotes
 import json
 
-kb=kbh.KBHit()
 symbol=raw_input("Enter Company Symbol:");
 m= re.search(r'"LastTradeWithCurrency":.*?Rs\.(.*?)"',json.dumps(getQuotes(symbols=symbol),indent=2))
 print m.group(1)
-while True:
-    key==kb.kbhit()
-    if (key==27):
-        break;
-    time.sleep(1)
-    print m.group(1)
